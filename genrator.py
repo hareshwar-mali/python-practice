80d3ff9fb33f9d95858f648044b62abc00050cff
Hareshwar <hareshwarmali20@gmail.com>
2024-12-29 15:38:30 +0530

    use yield keyword

---
+++ genrator.py
@@ -0,0 +1,14 @@
+def sq_numbers(n):
+    for i in range(1, n+1):
+        print('okk')
+        yield i*i
+
+
+a = sq_numbers(3)
+
+print("The square of numbers 1,2,3 are : ")
+print(next(a))
+print(next(a))
+# print(next(a))
+
+
