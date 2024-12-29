e56cc3ef93369efb34c2710d71993010048eb3d2
Hareshwar <hareshwarmali20@gmail.com>
2024-12-29 15:38:55 +0530

    use next for iterator

---
+++ iterator.py
@@ -0,0 +1,9 @@
+
+my_list = [1, 2, 3, 4]
+iterator = iter(my_list)
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+for i in iterator:
+    print(i)
