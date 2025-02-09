d17699a19c1175c83dd877fa889a289bb714e9e2
Hareshwar <hareshwarmali20@gmail.com>
2025-02-09 16:33:36 +0530

    use lambda function

--- first_last_upper.py
+++ first_last_upper.py
@@ -11,3 +11,13 @@ def capitalize_first_last(s):
 
 s = "Write Python code in this online editor"
 print(capitalize_first_last(s))
+
+# using lamda function
+res = ' '.join(
+    map(
+        lambda word: word[0].upper() + word[1:-1] + word[-1].upper()  # Capitalize first and last character
+        if len(word) > 1 else word.upper(),  # If word length is 1, capitalize the whole word
+        s.split()  # Split `s`into words
+    )
+)
+print(res)
