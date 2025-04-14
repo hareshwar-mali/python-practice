b80d0f93f37e1fb7d29c050529d9832ebfe5c218
Hareshwar <hareshwarmali20@gmail.com>
2025-04-14 21:04:44 +0530

    binary search and get list index

---
+++ binary_search.py
@@ -0,0 +1,18 @@
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+arr = [1,11,21,31,41,51,61]
+target = 51
+result = binary_search(arr, target)
+
+print(result)
